7193cfadf79406b2d72c1fba87b587cbb06c6cee
Mert Cengiz <118610270+MertCengiz@users.noreply.github.com>
2023-05-15 23:46:31 +0300

    Update widget.cpp

--- Trial-2/widget.cpp
+++ Trial-2/widget.cpp
@@ -20,29 +20,48 @@ PairMatch::PairMatch(QWidget *parent) : QWidget(parent) {
     connect(newGameButton, &QPushButton::clicked, this, &PairMatch::Refresh);
     // Refresh everything (Restart the game)
 
+    QPushButton *buttons[5][6];   // Store the buttons in order to change it when necessary.
+    QString randomNames[5][6];
+
     for (int i = 1; i <= 5; i++){
         for (int j = 0; j <= 5; j++){
-            // TODO: Give random texts to each pair of buttons
-            auto *questionMark = new QPushButton("?", this);
-            connect(questionMark, &QPushButton::clicked, this, &PairMatch::Increment);
-            connect(questionMark, &QPushButton::clicked, this, &PairMatch::Decrement);
-            grid->addWidget(questionMark, i, j);
+            QString randomString = GenerateRandomMatrices(used, elements);
+            randomNames[i - 1][j] = randomString;
+            buttons[i - 1][j] = new QPushButton("?", this);
+            connect(buttons[i - 1][j], &QPushButton::clicked, this, &PairMatch::Increment);
+            connect(buttons[i - 1][j], &QPushButton::clicked, this, &PairMatch::Decrement);
+            grid->addWidget(buttons[i - 1][j], i, j);
         }
     }
+
+    // TODO: When pressed, change the button with its version containing name from its index.
+    // TODO: Make refresh every button question mark.
+
+    /*buttons[2][1] = new QPushButton(randomNames[2][1], this);     // TESTING STUFF, NOT IMPORTANT!
+    connect(buttons[2][1], &QPushButton::clicked, this, &PairMatch::Increment);
+    connect(buttons[2][1], &QPushButton::clicked, this, &PairMatch::Decrement);
+    grid->addWidget(buttons[2][1], 2, 1);*/
 }
 
 void PairMatch::Increment() {
 
     int val = scoreCount->text().toInt();
-    val++;  // TODO: Increment only when textEqual == 1
+    QString text = scoreCount->text();
+    if (pushOrder % 2 == 0){
+        previousString = text;
+    }
+    else if (pushOrder % 2 == 1){
+        textEqual = QString::compare(previousString, text, Qt::CaseInsensitive);
+        if (textEqual == 0) // TODO: Fix the bug in comparing (Always executes this if block)
+            val++;
+    }
     scoreCount->setText(QString::number(val));
-    // TODO: If previousString and current text are the same, make textEqual == 1
 }
 
 void PairMatch::Decrement() {
 
     int val = remCount->text().toInt();
-    if (pushOrder % 2 == 1 && textEqual == 0)
+    if (pushOrder % 2 == 1 && pushOrder > 0)
         val--;  // Decrease val if the two string are not the same.
     pushOrder++;
     remCount->setText(QString::number(val));
@@ -57,4 +76,14 @@ void PairMatch::Refresh() {
     scoreCount->setText(QString::number(valScore));
     remCount->setText(QString::number(remScore));
     // TODO: Refresh the question marks.
+}
+
+QString PairMatch:: GenerateRandomMatrices(int *used, QString *elements){
+    quint32 randIndex = QRandomGenerator::global()->bounded(15);
+    QString stringToWrite;
+    while (used[randIndex] >= 2){
+       randIndex = QRandomGenerator::global()->bounded(16);
+    }
+    stringToWrite = elements[randIndex];
+    return stringToWrite;
 }
